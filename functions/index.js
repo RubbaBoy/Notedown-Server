576be7ea85f317015bffa20e1066fd074e3388ac
RubbaBoy <adamyarris@gmail.com>
2019-11-28 10:39:10 -0500

    Added basic functionality of note and collection creation/editing

--- functions/index.js
+++ functions/index.js
@@ -1,6 +1,7 @@
 const functions = require('firebase-functions');
 const admin = require('firebase-admin');
 const serviceAccount = require('./serviceAccountKey.json');
+const emptyData = {categories: [], notes: []};
 
 admin.initializeApp({
     credential: admin.credential.cert(serviceAccount),
@@ -9,45 +10,70 @@ admin.initializeApp({
 let db = admin.firestore();
 
 exports.addCategory = functions.https.onCall((input, context) => {
-    const uid = context.auth.uid;
-    const name = context.auth.token.name || null;
-
-    let uidDoc = db.collection('users').doc(uid);
-
     return new Promise((resolve, reject) => {
+        if (input.name === undefined) {
+            resolve({success: false, error: 'Field \'name\' must be set'});
+            return;
+        }
+
+        let uidDoc = db.collection('users').doc(context.auth.uid);
         uidDoc.get().then(snapshot => {
-            let data = snapshot.data();
-            console.log(data);
-            if (data === undefined) {
-                data = {categories: []};
-            }
+            let data = snapshot.data() || {categories: []};
+            let id = uuidv4();
             data.categories = data.categories || [];
-            data.categories.push({id: uuidv4(), name: input.name});
+            data.categories.push({id: id, name: input.name});
             uidDoc.set(data)
-                .then(() => resolve({message: `Hello ${name}! Your data is ${input.id} and ${input.name}`}))
+                .then(() => resolve({success: true, id: id}))
                 .catch(rej => {
                     console.error(rej);
-                    reject({message: 'Error!', error: rej.toString()});
+                    resolve({success: false, error: rej.toString()});
                 });
         }).catch(rej => {
             console.error(rej);
-            reject({message: 'Error!', error: rej.toString()});
+            resolve({success: false, error: rej.toString()});
         });
     });
 });
 
 exports.getCategories = functions.https.onCall((input, context) => {
-    const uid = context.auth.uid;
-
     return new Promise((resolve, reject) => {
-        let uidDoc = db.collection('users').doc(uid);
+        let uidDoc = db.collection('users').doc(context.auth.uid);
         uidDoc.get().then(snapshot => {
-            let data = snapshot.data();
-            let cat = data !== undefined ? (data.categories || []) : [];
-            resolve({categories: cat});
+            let data = snapshot.data() || emptyData;
+            resolve({success: true, categories: data.categories || []});
         }).catch(rej => {
             console.error(rej);
-            reject({categories: [], error: rej.toString()});
+            resolve({success: false, error: rej.toString(), categories: []});
+        });
+    });
+});
+
+exports.editNote = functions.https.onCall((input, context) => {
+    return new Promise((resolve, reject) => {
+        let uid = context.auth.uid;
+        let notesCollection = db.collection('users').doc(uid).collection('notes');
+        let creating = input.id === undefined;
+        let editingId = input.id || uuidv4();
+        let categoryId = input.category;
+        let newTitle = input.title;
+        let newContent = input.content;
+
+        let editingDocument = notesCollection.doc(editingId);
+        editingDocument.get().then(snapshot => {
+            let data = snapshot.data() || {};
+            if (newTitle !== undefined) data.title = newTitle;
+            if (newContent !== undefined) data.content = newContent;
+            if (creating || categoryId !== undefined) data.categoryId = categoryId || '';
+
+            editingDocument.set(data)
+                .then(() => resolve({success: true, id: editingId}))
+                .catch(rej => {
+                    console.error(rej);
+                    resolve({success: false, error: rej.toString()});
+                });
+        }).catch(rej => {
+            console.error(rej);
+            resolve({success: false, error: rej.toString()});
         });
     });
 });
